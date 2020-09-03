0272e1125502872db4c13638782591201f4794ab
Alessio <alessiohall@gmail.com>
2020-09-03 01:11:23 -0500

    Got everything working except for the 3 decimal places rule. Sum and multiplication work

--- Participations/Participation1_SumOf3Numbers/Program.cs
+++ Participations/Participation1_SumOf3Numbers/Program.cs
@@ -2,15 +2,31 @@
 
 namespace Participation1_SumOf3Numbers
 
-    // prompt the user to enter 3 diff numbers (decimal). Add all 3 numbers together and output result with 3 decimal places
-    // Calculate a second value where you multiply sum ^ by constant 7.777.
+    // Figure out the 3 decimal thing
 {
     class Program
     {
-        const double LUCKY_NUMBER = 7.777;
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("In numbers only, how old are you?");
+            double Years = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Once again, in numbers only, what is your shoe size (decimals allowed on this one)?");
+            double Shoe = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Lastly, what is your most favorite number?");
+            double FavNumber = Convert.ToDouble(Console.ReadLine());
+
+            double sum = Years + Shoe + FavNumber;
+            Console.WriteLine("The sum of the three numbers you just provided me is " + sum + "!");
+
+            const double seven = 7.777;
+            double MagicNumber = sum * seven;
+
+            Console.WriteLine("With a bit of magic, your new lucky number is " + MagicNumber + "!");
+            Console.WriteLine("How cool!");
+
+
         }
     }
 }
