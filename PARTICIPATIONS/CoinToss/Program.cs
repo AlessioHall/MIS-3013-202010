36b967e1a89a5e772a8b03919c8524b5238506db
Alessio <alessiohall@gmail.com>
2020-09-10 14:41:23 -0500

    Finished - conditional statements are a bit messy but they work. Need to learn how to work with capitalizations on user responses.

--- PARTICIPATIONS/CoinToss/Program.cs
+++ PARTICIPATIONS/CoinToss/Program.cs
@@ -8,15 +8,67 @@ namespace CoinToss
 {
     class Program
     {
+        
+        const string DEVELOPER = "Alessio Hall";
+        const int HEADS = 1;
+        const int TAILS = 0;
+
         static void Main(string[] args)
+
         {
             Random rnd = new Random();
-
             int coin = rnd.Next(0, 2);
 
+            Console.WriteLine("Pick one: Heads or Tails (capitalize first letter please) >>");
+
+            Console.WriteLine();
+
+            string response = Console.ReadLine();
+                 
+            Console.WriteLine();
             
 
+            if (coin == HEADS)
+            {
+                Console.WriteLine("It landed on Heads");
 
+                if (response == "Heads")
+                {
+                    Console.WriteLine("You got it! Congrats!");
+                }
+                else
+                {
+                    Console.WriteLine("WRONG! TRY AGAIN");
+                }
+            }
+
+            else if (coin == TAILS)
+            {
+                Console.WriteLine("It landed on Tails");
+                if (response == "Tails")
+                {
+                    Console.WriteLine("You got it! Congrats!");
+                }
+                else
+                {
+                    Console.WriteLine("WRONG! TRY AGAIN");
+                }
+            }
+            else
+            {
+                return;
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine(DEVELOPER);
+            Console.ReadKey();
+
+                }
+
+            }
+        
         }
-    }
-}
+    
+
