a4adb8ae298a8412183e622aec8fbf812971b72e
Alessio <alessiohall@gmail.com>
2020-09-10 12:31:50 -0500

    Finished FizzBuzz with the conditional statements. Tested it by displaying the number first and then the required output and it seemed to work for all 3 scenarios

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 //Generate a random number with a range between 1 and 100, using the Random (Links to an external site.) class.  Once you have the random number generated, we will play the FizzBuzz Game.
 //If the number they provided to you is divisible by 3, then you should output the word 'Fizz'.
@@ -6,7 +7,7 @@
 //If the number is divisible by 3 AND 5, then you should output the word 'Fizz Buzz'
 //If the number is NOT divisible by either, just output the number back out. 
 
-
+//BOOLEAN OPERATORS: OR = ||, AND = &&
 
 namespace FizzBuzz
 {
@@ -18,11 +19,28 @@ namespace FizzBuzz
             Random random = new Random();
 
             int randnumber = random.Next(1, 100);
-            Console.WriteLine("Random Number Generator = " + randnumber);
+            int remainder = randnumber % 3;
+            int remainder2 = randnumber % 5;
 
+            
+            if (remainder == 0 && remainder2 == 0)
+            {
 
+                Console.WriteLine("FizzBuzz");
 
-
+            }
+            else if (remainder == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (remainder2 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(randnumber);
+            }
 
 
         }
