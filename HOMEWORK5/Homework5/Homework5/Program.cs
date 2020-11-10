0cb6aa81101650e9749bd927f91b6338f20612ed
Alessio <alessiohall@gmail.com>
2020-11-10 03:21:14 -0600

    finished lists and array - can't seem to figure out the dictionaries part

---
+++ HOMEWORK5/Homework5/Homework5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string AcctBalances = @"C:\Users\aless\Downloads\AccountBalances.csv";
+            string[] balance = File.ReadAllLines(AcctBalances);
+
+            string CustomerNames = @"C:\Users\aless\Downloads\CustomerNames.csv";
+            string[] customers = File.ReadAllLines(CustomerNames);
+
+            foreach (var name in customers)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Type out a customer's name to see their current balance --");
+            Console.WriteLine("*Make sure to use correct capitalizations!*");
+            Console.WriteLine();
+
+            string customername = Console.ReadLine();
+            
+
+            for (int i = 0; i < customers.Length; i++)
+            {
+                string name = customers[i];
+                if (name == customername)
+                {
+                    string accountbalance = balance[i];
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine($"{customername} has a current balance of {accountbalance}");
+                }
+            }
+
+
+
+            Console.ReadKey();
+         }
+    }
+}
---
+++ HOMEWORK5/Homework5/Lists/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> favthings = new List<string>();
+
+            string answer;
+            do
+            {
+                Console.WriteLine("enter a fav thing");
+                favthings.Add(Console.ReadLine());
+                Console.WriteLine("do u wanna add another");
+                answer = Console.ReadLine();
+            } while (answer.ToLower() == "yes");
+
+            Console.WriteLine();
+            Console.WriteLine("Here is your list in the order that you typed it -- ");
+            for (int i = 0; i < favthings.Count; i++)
+            {
+                if (i != favthings.Count -1)
+                {
+                Console.WriteLine($"{favthings[i]}, ");
+                }
+                else
+                {
+                    Console.WriteLine($"{favthings[i]}");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Here is your list but backwards -- ");
+            for (int i = favthings.Count - 1; i >= 0; i--)
+            {
+                if (i != 0)
+                {
+                    Console.WriteLine($"{favthings[i]}, ");
+                }
+                else
+                {
+                    Console.WriteLine($"{favthings[i]}");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Finally, here is every other thing you typed");
+            for (int i = 0; i < favthings.Count; i += 2)
+            {
+                if (i != favthings.Count - 2)
+                {
+                    Console.WriteLine($"{favthings[i]}, ");
+                }
+                else
+                {
+                    Console.WriteLine($"{favthings[i]}");
+                }
+            }
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
