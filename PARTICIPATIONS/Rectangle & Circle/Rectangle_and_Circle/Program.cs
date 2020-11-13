a6a557eee3978b5cbb08845edf6c792773a9cbde
Alessio <alessiohall@gmail.com>
2020-11-13 01:54:59 -0600

    finished rectangle and circles participation

--- PARTICIPATIONS/Rectangle & Circle/Rectangle_and_Circle/Program.cs
+++ PARTICIPATIONS/Rectangle & Circle/Rectangle_and_Circle/Program.cs
@@ -22,7 +22,16 @@ namespace Rectangle_and_Circle
             Length = length;
             Width = width;
         }
-                    
+
+        public double CalculateArea()
+        {
+            return Width * Length;
+        }
+
+        public double CalculatePerimeter()
+        {
+            return 2 * (Length + Width);
+        }
     }
     class Circle
     {
@@ -53,6 +62,33 @@ namespace Rectangle_and_Circle
     {
         static void Main(string[] args)
         {
+
+            Circle myCircle = new Circle();
+            myCircle.Radius = 7.5;
+            Console.WriteLine($"A circle with radius {myCircle.Radius} has an area of {myCircle.CalculateArea()}\n" + $" and a perimeter of {myCircle.CalculatePerimeter()}");
+
+            Circle circle2 = new Circle();
+            circle2.Radius = 101010;
+            double areaofcircle2 = circle2.CalculateArea();
+            double perimeterofcircle2 = circle2.CalculatePerimeter();
+
+            Circle circle3 = new Circle(3.5);
+
+            Rectangle myRectangle = new Rectangle();
+            myRectangle.Length = 18;
+            myRectangle.Width = 4;
+            Console.WriteLine($"A rectangle with length {myRectangle.Length} and width {myRectangle.Width} has an area of {myRectangle.CalculateArea()} and a perimeter of {myRectangle.CalculatePerimeter()}");
+
+            Rectangle rectangle2 = new Rectangle();
+            rectangle2.Length = 41;
+            rectangle2.Width = 30;
+            double areaofrectangle2 = rectangle2.CalculateArea();
+            double perimeterofrectangle2 = rectangle2.CalculatePerimeter();
+
+            Rectangle rectangle3 = new Rectangle(9, 14);
+
+
+            Console.ReadKey();
         }
     }
 }
