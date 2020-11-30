c3aa5f470d1cb7ca8ee8d2e058444c6103509b84
Alessio <alessiohall@gmail.com>
2020-11-30 13:54:11 -0600

    finished textbox participation - going to try later and see if it can be done in a different way

---
+++ PARTICIPATIONS/TextBox/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBox
+{
+    public class TextBox
+    {
+        public int Padding { get; set; }
+
+        public ConsoleColor BackColor { get; set; }
+
+        public ConsoleColor ForeColor { get; set; }
+
+        public string Text { get; set; }
+
+        public TextBox()
+        {
+
+        }
+
+        public void DisplayText()
+        {
+            Console.BackgroundColor = this.BackColor;
+            Console.ForegroundColor = this.ForeColor;
+            string stringpadding = new string(' ', this.Padding);
+            Console.WriteLine(stringpadding + this.Text + stringpadding);
+
+        }
+    }
+}
---
+++ PARTICIPATIONS/TextBox/Class2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBox
+{
+    class Class2
+    {
+        static void Main(string[] args)
+        {
+            TextBox firsttextbox = new TextBox()
+            {
+                Text = "This is the first textbox - it has a green forecolor, a red backcolor, and a padding of 2",
+                BackColor = ConsoleColor.Red,
+                ForeColor = ConsoleColor.Green,
+                Padding = 2,
+            };
+
+            TextBox secondtextbox = new TextBox()
+            {
+                Text = "This is the second textbox - it has a yellow forecolor, a dark magenta backcolor, and a padding of 50",
+                BackColor = ConsoleColor.DarkMagenta,
+                ForeColor = ConsoleColor.Yellow,
+                Padding = 50,
+            };
+
+            firsttextbox.DisplayText();
+
+            secondtextbox.DisplayText();
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
